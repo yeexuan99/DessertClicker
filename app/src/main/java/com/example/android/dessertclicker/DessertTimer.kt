8b19431f79ab83874aaf02e79f7d2a9e28f44b9b
yeexuan99 <52269362+yeexuan99@users.noreply.github.com>
2020-01-04 11:55:33 +0800

    Update DessertTimer.kt

--- app/src/main/java/com/example/android/dessertclicker/DessertTimer.kt
+++ app/src/main/java/com/example/android/dessertclicker/DessertTimer.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright 2019, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 package com.example.android.dessertclicker
 
@@ -22,21 +8,7 @@ import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
 import timber.log.Timber
 
-/**
- * This is a class representing a timer that you can start or stop. The secondsCount outputs a count of
- * how many seconds since it started, every one second.
- *
- * -----
- *
- * Handler and Runnable are beyond the scope of this lesson. This is in part because they deal with
- * threading, which is a complex topic that will be covered in a later lesson.
- *
- * If you want to learn more now, you can take a look on the Android Developer documentation on
- * threading:
- *
- * https://developer.android.com/guide/components/processes-and-threads
- *
- */
+
 class DessertTimer(lifecycle: Lifecycle) : LifecycleObserver {
 
     // The number of seconds counted since the timer started
